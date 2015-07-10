2ab33747e44df1d692973151657bfab024c950ac
Nicholas Mulvenna <mulvenna@linux.com>
2015-07-09 19:26:47 -0700

    decoder_3in_8out

---
+++ decoder_3in_8out.vhdl
@@ -0,0 +1,28 @@
+-- summer 2015 assignment 1 question 1
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity decoder is
+  port(w : in std_logic_vector(2 downto 0);
+       y : out std_logic_vector(7 downto 0));
+end entity;
+
+--The decoder asserts (sets to 1) bit w of y
+--when the input w is "000", the output y is "00000001"
+--when the input w is "100", the output y is "00010000"
+architecture behavioural of decoder is
+begin
+  process(w)
+  begin
+    case w is
+      when "000"  => y <= "00000001";
+      when "001"  => y <= "00000010";
+      when "010"  => y <= "00000100";
+      when "011"  => y <= "00001000";
+      when "100"  => y <= "00010000";
+      when "101"  => y <= "00100000";
+      when "110"  => y <= "01000000";
+      when others => y <= "10000000";
+    end case;
+  end process;
+end behavioural;
---
+++ decoder_3in_8out_testbench.vhdl
@@ -0,0 +1,42 @@
+-- summer 2015 assignment 1 question 1
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity decoder_testbench is
+end entity;
+
+architecture stimulus of decoder_testbench is
+  component decoder is
+    port (w : in std_logic_vector(2 downto 0);
+          y : out std_logic_vector(7 downto 0));
+  end component;
+  
+  signal in_w : std_logic_vector(2 downto 0);
+  signal out_y : std_logic_vector(7 downto 0);
+  signal expected_y : std_logic_vector(7 downto 0);
+  constant WAIT_TIME : time := 5ns;
+  
+begin
+  DUT : decoder port map (w => in_w, y => out_y);
+  
+  test : process
+  begin
+    in_w <= "000";
+    expected_y <= "00000001";
+    
+    for i in 0 to 7 loop
+      wait for WAIT_TIME;
+      assert (out_y = expected_y)
+        report "assert error at iteration " & integer'image(i)
+        severity error;
+      in_w <= std_logic_vector(unsigned(in_w) + 1); --increment
+      expected_y <= expected_y(6 downto 0) & '0'; --shift left 1 bit
+    end loop;
+    
+    report "Tests completed without errors"
+      severity note;
+    wait; --done
+    
+  end process;
+end architecture;
